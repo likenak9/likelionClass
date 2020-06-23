7fe08f7602004853e1e31d52ced736e5bd0f3f87
lsw <nak9nom@likelion.org>
2020-06-23 12:44:35 +0000

    endword modify

--- third.py
+++ third.py
@@ -7,11 +7,12 @@ word1 = input("끝말잇기를 시작합시다 : ")
 
 if len(word1) == 3:
     pre_word = word1
+    print(pre_word[-1] + "로 시작되는 단어를 입력하세요.")
     while True:
         word2 = input()
     
         if (len(word2) == 3) and (word2[0] == pre_word[-1]):
-            print("정답입니다. 다음 단어를 입력하세요.")
+            print("정답입니다." + word2[-1] + "로 시작되는 단어를 입력하세요.")
             pre_word = word2
         else:
             print("오답입니다.")
@@ -19,7 +20,6 @@ if len(word1) == 3:
         
 else:
     print("3글자만 가능합니다.")
-djksjdks
 
     
 # def endwords(a):
